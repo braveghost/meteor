04194eed89c5a9e8e20d3b6a829245e3c280660e
Miller <chailunlun@superatomfin.com>
2019-10-17 15:37:45 +0800

    修改mode

--- errutil/errutil.go
+++ errutil/errutil.go
@@ -1,7 +1,13 @@
 package errutil
 
+import "github.com/pkg/errors"
+
 func CheckErrPanic(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func Is(err, wrapErr error) bool {
+	return err == errors.Cause(wrapErr)
 }
--- mode/mode.go
+++ mode/mode.go
@@ -5,54 +5,97 @@ import (
 )
 
 var (
-	ModeSetError = errors.New("mode setting error, ['pro','dev','local','sta','test']")
+	ModeSetNameError = errors.New("mode setting error, ['pro','dev','local','sta','test']")
+	ModeSetNumError  = errors.New("mode setting error, [1,2,3,3,5]")
+	ModeNameError    = errors.New("mode name error, ['pro','dev','local','sta','test']")
 )
-
-type ModeType string
 
 var (
-	ModePro   = ModeType("pro")
-	ModeDev   = ModeType("dev")
-	ModeLocal = ModeType("local")
-	ModeSta   = ModeType("sta")
-	ModeTest  = ModeType("test")
+	modeName = map[ModeType]string{
+		ModeLocal: "local",
+		ModeDev:   "dev",
+		ModeTest:  "test",
+		ModeStage: "stage",
+		ModePro:   "pro",
+	}
 
-	ModeList        = []ModeType{ModePro, ModeDev, ModeLocal, ModeSta, ModeTest}
-	ModesOnlineList = []ModeType{ModePro, ModeDev, ModeSta, ModeTest}
-	modeS           string
-	modeT           ModeType
+	modeValue = map[string]ModeType{
+		"local": ModeLocal,
+		"dev":   ModeDev,
+		"test":  ModeTest,
+		"stage": ModeStage,
+		"pro":   ModePro,
+	}
 )
 
-func InModesList(md ModeType, li []ModeType) bool {
-	for _, m := range li {
-		if md == m {
-			return true
-		}
+type ModeType int
+
+func (mt ModeType) String() string {
+	return modeName[mt]
+}
+
+func (mt ModeType) Int() int {
+	return int(mt)
+}
+
+func (mt ModeType) Int32() int32 {
+	return int32(mt)
+}
+
+func (mt ModeType) Int64() int64 {
+	return int64(mt)
+}
+
+const (
+	ModeLocal ModeType = iota + 1 // localhost
+	ModeDev                       // development
+	ModeTest                      // test
+	ModeStage                     // stage
+	ModePro                       // production
+)
+
+var (
+	defaultMode = ModeLocal
+)
+
+func IsOnline(md ModeType) bool {
+	return md != ModeLocal
+}
+
+func SetModeByNum(m int) error {
+	tmp := ModeType(m)
+
+	if _, ok := modeName[tmp]; ! ok {
+		return ModeSetNumError
 	}
-	return false
-}
-
-func InModesOnline(md ModeType) bool {
-	return InModesList(md, ModesOnlineList)
-
-}
-
-func InModes(md ModeType) bool {
-	return InModesList(md, ModeList)
-}
-
-func SetMode(m string) error {
-	modeS, modeT = m, ModeType(m)
-	if !InModes(modeT) {
-		return ModeSetError
-	}
+	defaultMode = tmp
 	return nil
 }
 
-func GetModeString() string {
-	return modeS
+func SetModeByName(name string) error {
+	m, ok := modeValue[name]
+	if ! ok {
+		return ModeSetNameError
+	}
+	defaultMode = m
+	return nil
 }
 
-func GetMode() ModeType {
-	return modeT
+func GetDefaultMode() ModeType {
+	return defaultMode
+}
+
+func GetModeByName(name string) (ModeType, error) {
+	if m, ok := modeValue[name]; ! ok {
+		return m, nil
+	}
+	return -1, ModeNameError
+}
+
+func GetModeByNum(m int) (ModeType, error) {
+	tmp := ModeType(m)
+	if _, ok := modeName[tmp]; ! ok {
+		return tmp, nil
+	}
+	return -1, ModeNameError
 }
